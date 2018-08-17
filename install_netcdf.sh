7ca1ef92110d28504ed2388bb816324fd9c99cd8
Daniel Santiago <dspelaez@gmail.com>
2018-08-17 15:31:26 -0700

    curl added and stderr output redirected to config.log file

--- install_netcdf.sh
+++ install_netcdf.sh
@@ -35,25 +35,42 @@ F77=${FC}
 MAINDIR=${MAINDIR:-/usr/local/netcdf}
 
 # version of libs
+CLTAG="7.61.0"
 ZLTAG="1.2.10"
 H5TAG="1.10.1"
 NCTAG="4.6.1"
 NFTAG="4.4.4"
 
 ## donwload source code of depencies
-wget -nc -nv https://zlib.net/fossils/zlib-$ZLTAG.tar.gz
-wget -nc -nv https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-$H5TAG/src/hdf5-$H5TAG.tar 
-wget -nc -nv ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-$NCTAG.tar.gz
-wget -nc -nv ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-fortran-$NFTAG.tar.gz
+wget -nc -nv https://curl.haxx.se/download/curl-$CLTAG.tar.gz
+#wget -nc -nv https://zlib.net/fossils/zlib-$ZLTAG.tar.gz
+#wget -nc -nv https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-$H5TAG/src/hdf5-$H5TAG.tar 
+#wget -nc -nv ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-$NCTAG.tar.gz
+#wget -nc -nv ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-fortran-$NFTAG.tar.gz
+
+## create config.log
+touch config.log
+
+## curl
+tar -xf curl-$CLTAG.tar.gz
+cd curl-$CLTAG/
+CLDIR=$MAINDIR
+echo " --->> Compiling curl-$CLTAG"
+./configure --prefix=${CLDIR} > config.log 2>&1
+make -j4 > config.log 2>&1
+make install > config.log 2>&1
+cd ..
+rm -rf curl-$CLTAG
+
 
 ## zlib 
 tar -xf zlib-$ZLTAG.tar.gz
 cd zlib-$ZLTAG/
 ZDIR=$MAINDIR
-echo " --->> Compiling Zlib-$ZLTAG"
-./configure --prefix=${ZDIR} > /dev/null 2>&1
-make -j4 > /dev/null 2>&1
-make install > /dev/null 2>&1
+echo " --->> Compiling zlib-$ZLTAG"
+./configure --prefix=${ZDIR} > config.log 2>&1
+make -j4 > config.log 2>&1
+make install > config.log 2>&1
 cd ..
 rm -rf zlib-$ZLTAG
 
@@ -62,9 +79,9 @@ tar -xf hdf5-$H5TAG.tar
 cd hdf5-$H5TAG/
 H5DIR=$MAINDIR
 echo " --->> Compiling hdf5-$H5TAG"
-./configure --with-zlib=${ZDIR} --prefix=${H5DIR} > /dev/null 2>&1
-make -j4 > /dev/null 2>&1
-make install > /dev/null 2>&1
+./configure --with-zlib=${ZDIR} --prefix=${H5DIR} > config.log 2>&1
+make -j4 > config.log 2>&1
+make install > config.log 2>&1
 cd ..
 rm -rf hdf5-$H5TAG
 
@@ -73,9 +90,9 @@ tar -xf netcdf-$NCTAG.tar.gz
 cd netcdf-$NCTAG/
 NCDIR=$MAINDIR
 echo " --->> Compiling netcdf-$NCTAG"
-CPPFLAGS=-I${H5DIR}/include LDFLAGS=-L${H5DIR}/lib ./configure --prefix=${NCDIR} > /dev/null 2>&1
-make -j4 > /dev/null 2>&1
-make install > /dev/null 2>&1
+CPPFLAGS=-I${H5DIR}/include LDFLAGS=-L${H5DIR}/lib ./configure --prefix=${NCDIR} > config.log 2>&1
+make -j4 > config.log 2>&1
+make install > config.log 2>&1
 cd ..
 rm -rf netcdf-$NCTAG
 
@@ -83,9 +100,9 @@ rm -rf netcdf-$NCTAG
 tar -xf netcdf-fortran-$NFTAG.tar.gz
 cd netcdf-fortran-$NFTAG/
 echo " --->> Compiling netcdf-fortran-$NFTAG"
-CPPFLAGS=-I${NCDIR}/include LDFLAGS=-L${NCDIR}/lib ./configure --prefix=${NCDIR} > /dev/null 2>&1
-make -j4 > /dev/null 2>&1
-make install > /dev/null 2>&1
+CPPFLAGS=-I${NCDIR}/include LDFLAGS=-L${NCDIR}/lib ./configure --prefix=${NCDIR} > config.log 2>&1
+make -j4 > config.log 2>&1
+make install > config.log 2>&1
 cd ..
 rm -rf netcdf-fortran-$NFTAG
 
